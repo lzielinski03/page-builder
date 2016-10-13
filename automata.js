22896daa1c9b03d4f85620eb5e5f5f9790914109
lzielinski03 <lzielinski03@gmail.com>
2016-10-12 22:10:09 -0300

    automata structure

--- automata.js
+++ automata.js
@@ -50,3 +50,46 @@ const SearchNew: {
 		box: {}
 	]
 }
+
+
+1+	copiar proyecto 							
+2+	borrar contenido del source 				
+3+	generar manualmente la metadata 			
+4+	compilar manualmente en la carpeta source	
+
+5-  agregar Componentes a la metadata
+6-	Compilar nuevamente
+7-	actualizar metadata para conveniencia del compilador
+8-	
+
+<script>
+const metadata = Layer: {
+	properties: {},
+	type: 'element',
+	childs: [
+		Box: {
+			properties: {
+				backgroundColor: 'blue'
+			},
+			type: 'element',
+			classNames: ['default-box'],
+			childs: []
+		},
+		Box: {
+			properties: {},
+			type: 'element',
+			classNames: ['default-box'],
+			childs: []
+		}
+	]
+}
+</script>
+
+
+return CompiledReactHtml = Compiler(metadata)
+
+
+<div className="layer">
+	<div className="default-box" style={{'backgroundColor': 'blue'}}></div>
+	<div className="default-box"></div>
+</div>
