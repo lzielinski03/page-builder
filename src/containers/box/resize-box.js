2e468a3979a748e9cfcbdb955b73f6a9a1f89823
lzielinski03 <lzielinski03@gmail.com>
2016-12-06 15:23:06 -0300

    resize prototype working

--- src/containers/box/resize-box.js
+++ src/containers/box/resize-box.js
@@ -4,11 +4,42 @@ import Box from './../../components/box'
 export default class ResizableBox extends Component {
 	constructor(props) {
 		super(props);
+		this.handleRight = this.handleRight.bind(this)
+		this.finish = this.finish.bind(this)
+		this.move = this.move.bind(this)
+		this.state = {width: 100}
 	}
+
+	
 
 	handleRight(e) {
 		console.log('right')
-		console.log(e)
+		console.log(e.type)
+		/*
+		console.log('client')
+		console.log(e.clientX)
+		console.log(e.clientY)
+		console.log('page')
+		console.log(e.pageX)
+		console.log(e.pageY)
+		console.log('page')
+		console.log(e.screenX)
+		console.log(e.screenY)*/
+		document.addEventListener('mouseup', this.finish, true)
+		document.addEventListener('mousemove', this.move, true)
+	}
+
+	finish() {
+		document.removeEventListener('mouseup', this.finish, true)
+		document.removeEventListener('mousemove', this.move, true)
+		console.log('end')
+	}
+
+	move(e) {
+		console.log(e.clientX)
+		this.setState({width: e.clientX})
+		console.log(e.clientY)
+		console.log()
 	}
 
 	render() {
@@ -17,9 +48,11 @@ export default class ResizableBox extends Component {
 		let finalWidth = 100;
 		let width = finalWidth + 'px'
 		return (
-			<Box {...this.props} position="relative" width={width}>
+			<Box {...this.props} position="relative" width={this.state.width + 'px'}>
 				<div style={{'height':'100%', width:'10px', 'background-color':'white', 'position': 'absolute', right: '-5px'}} 
-					 onMouseDown={this.handleRight}></div>
+					onMouseDown={this.handleRight}
+					ref="resize"
+					 ></div>
 				{this.props.children}
 			</Box>
 		)
