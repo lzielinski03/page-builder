0da7ee7e4664b00bc91b9043b126de63c6af8970
lzielinski03 <lzielinski03@gmail.com>
2016-12-08 01:49:54 -0300

    deprecated code removed

--- src/containers/layer/actions.js
+++ src/containers/layer/actions.js
@@ -4,16 +4,8 @@ export const add = (id, child) => {
 	return { type: types.ADD_CHILD, id: id, child: child }
 }
 
-export const select = selected => {
-	return { type: types.SELECT_LAYER, selected }
-}
-
 export const toggleDirection = direction => {
 	return { type: types.TOGGLE_DIRECTION, direction }
-}
-
-export const selectElement = element => {
-	return { type: types.SELECT_ELEMENT, element }
 }
 
 export const changeColor = color => {
